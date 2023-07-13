981711860d9ad86c880cdf51f332fcac5eee6513
Crossroads30 <Crossroads30@mail.ru>
2023-07-13 16:26:39 +0300

    feat: add createView method

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,14 @@
 import '../style.css';
+import HeaderView from '../view/header/header-view';
 
 export default class AppView {
     constructor() {
         this.createView();
     }
 
-    createView() {}
+    createView() {
+        const header = new HeaderView();
+
+        document.body.append(header.getHtmlElement(header.configureViwe()));
+    }
 }
