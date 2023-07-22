2f24b678bb6950331b70af10bd085fefae5129af
Crossroads30 <Crossroads30@mail.ru>
2023-07-23 02:01:10 +0300

    feat: add videos imges & youtube-view

---
+++ src/view/main/videos-block/card-youtube-window/card-youtube-view.js
@@ -0,0 +1,45 @@
+import ElementCreator from '../../../../util/element-creator';
+import CardView from '../card/card-view';
+import './card-youtube.css'
+
+const CssClasses = {
+    CONTAINER: 'card',
+    CONTAINER_FULL: 'card__full',
+    FIELD: 'card__field',
+    BUTTON: 'back__button',
+};
+const CARD_TEXT_BACK = 'Назад...';
+
+export default class CardDetailView extends CardView {
+    /**
+     * @override
+     */
+    configureView() {
+        this.viewElementCreator.setCssClasses([CssClasses.CONTAINER_FULL]);
+
+        /**
+         * @type {import('../../../../util/element-creator').ElementParams}
+         */
+        let labelParams = {
+            tag: 'div',
+            classNames: [CssClasses.FIELD],
+            textContent: '',
+            callback: null,
+        };
+        let creatorLabel = new ElementCreator(labelParams);
+        this.viewElementCreator.addInnerElement(creatorLabel);
+
+        creatorLabel.element.innerHTML = `<iframe src="${this.card.youtubeUrl}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+
+        const buttonParams = {
+            tag: 'button',
+            classNames: [CssClasses.BUTTON],
+            textContent: CARD_TEXT_BACK,
+            callback: this.buttonClickHandler.bind(this),
+        };
+        const creatorButton = new ElementCreator(buttonParams);
+        this.viewElementCreator.addInnerElement(creatorButton);
+    }
+
+    
+}
---
+++ src/view/main/videos-block/card/card-view.js
@@ -0,0 +1,75 @@
+import './card.css';
+import View from '../../../view';
+import ElementCreator from '../../../../util/element-creator';
+
+const CssClasses = {
+    CONTAINER: 'card',
+    IMG: 'card__image',
+    TITLE: 'card__title',
+};
+
+export default class CardView extends View {
+    /**
+     * @param {import('../../../../data/cards/cards').CardView} card
+     */
+    constructor(card) {
+        /**
+         * @type {import('../../../view').ViewParams}
+         */
+
+        const params = {
+            tag: 'a',
+            classNames: [CssClasses.CONTAINER],
+        };
+        super(params);
+
+        this.callback = null;
+        this.card = card;
+
+        this.configureView();
+    }
+
+    configureView() {
+        /**
+         * @type {import('../../../../util/element-creator').ElementParams}
+         */
+        const imgParams = {
+            tag: 'img',
+            classNames: [CssClasses.IMG],
+            textContent: '',
+            callback: this.buttonClickHandler.bind(this),
+        };
+        const creatorSongLink = new ElementCreator(imgParams);
+        this.viewElementCreator.addInnerElement(creatorSongLink);
+        creatorSongLink.element.setAttribute('src', `${this.card.url}`);
+        
+        /**
+         * @type {import('../../../../util/element-creator').ElementParams}
+         */
+        const titleParams = {
+            tag: 'p',
+            classNames: [CssClasses.TITLE],
+            textContent: this.card.name,
+            callback: this.buttonClickHandler.bind(this),
+        };
+        const creatorTitle = new ElementCreator(titleParams);
+        this.viewElementCreator.addInnerElement(creatorTitle);
+    }
+
+    /**
+     * @param {function} callback
+     */
+    setCallback(callback) {
+        if (typeof callback === 'function') {
+            this.callback = callback;
+        }
+    }
+
+    buttonClickHandler() {
+        this.callback();
+    }
+
+    getCardInfo() {
+        return this.card;
+    }
+}
