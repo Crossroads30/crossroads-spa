0e45497080af68831d74a8eac483e72c99440ba7
Crossroads30 <Crossroads30@mail.ru>
2023-07-26 10:38:06 +0300

    feat: add songs list section to html

---
+++ src/view/main/songs-block/songs-block-view.js
@@ -0,0 +1,72 @@
+import View from '../../view';
+import SongTextView from './song-text-window/song-text';
+import './songs-block.css';
+import SongView from './songs/songs-list-view';
+import songsInfo from '../../../data/songs/songs-list';
+
+const CssClasses = {
+    CONTEINER: 'songs-list',
+    HEADER: 'list-header',
+};
+
+export default class SongsBlock extends View {
+    constructor() {
+        /**
+         * @type {import('../../view').ViewParams}
+         */
+        const params = {
+            tag: 'ul',
+            classNames: [CssClasses.CONTEINER],
+            callback: Function,
+        };
+        super(params);
+
+        this.showAllSongs();
+    }
+
+    /**
+     * @param {import('../../../data/songs/songs-list').SongInfo} song
+     * @returns {SongView}
+     */
+    createSongsListToView(song) {
+        const songComponent = new SongView(song);
+        const callbackMoreInfo = () => this.showSongText(song);
+        songComponent.setCallback(callbackMoreInfo);
+        return songComponent;
+    }
+
+    /**
+     * @param {import('../../../data/songs/songs-list').SongInfo} song
+     * @returns {SongView}
+     */
+    createSongTextToView(song) {
+        const SongTextComponent = new SongTextView(song);
+        const callbackToSongs = () => this.showAllSongs();
+        SongTextComponent.setCallback(callbackToSongs);
+        return SongTextComponent;
+    }
+
+    showAllSongs() {
+        this.clearView();
+
+        songsInfo.forEach((song) => {
+            const songComponent = this.createSongsListToView(song);
+            this.viewElementCreator.addInnerElement(songComponent.getHtmlElement());
+        });
+        this.viewElementCreator.element.firstChild.innerHTML = '<p>НАЗВАНИЕ</p><p>АЛЬБОМ / ДАТА РЕЛИЗА</p>';
+        this.viewElementCreator.element.firstChild.classList.add('list-header');
+    }
+
+    showSongText(song) {
+        this.clearView();
+        const songText = this.createSongTextToView(song);
+        this.viewElementCreator.addInnerElement(songText.getHtmlElement());
+    }
+
+    clearView() {
+        const htmlElement = this.viewElementCreator.getElement();
+        while (htmlElement.firstElementChild) {
+            htmlElement.firstElementChild.remove();
+        }
+    }
+}
---
+++ src/view/main/songs-block/songs/songs-list-view.js
@@ -0,0 +1,73 @@
+import './songs-list.css';
+import View from '../../../view';
+import ElementCreator from '../../../../util/element-creator';
+
+const CssClasses = {
+    CONTAINER: 'song',
+    TITLE: 'song__info',
+    ALBUM: 'song__album',
+};
+
+export default class SongView extends View {
+    /**
+     * @param {import('../../../../data/songs/songs-list').SongData} song
+     */
+    constructor(song) {
+        /**
+         * @type {import('../../../view').ViewParams}
+         */
+        const params = {
+            tag: 'li',
+            classNames: [CssClasses.CONTAINER],
+            callback: Function,
+        };
+        super(params);
+
+        this.song = song;
+        this.callback;
+        this.configureView();
+    }
+
+    configureView() {
+        /**
+         * @type {import('../../../../util/element-creator').ElementParams}
+         */
+        const titleParams = {
+            tag: 'p',
+            classNames: [CssClasses.TITLE],
+            textContent: this.song.name,
+            callback: this.clickHandler.bind(this),
+        };
+        const creatorTitle = new ElementCreator(titleParams);
+        this.viewElementCreator.addInnerElement(creatorTitle);
+
+        /**
+         * @type {import('../../../../util/element-creator').ElementParams}
+         */
+        const album_releaseParams = {
+            tag: 'p',
+            classNames: [CssClasses.ALBUM],
+            textContent: this.song.album + ' / ' + this.song.release,
+            callback: this.clickHandler.bind(this),
+        };
+        const creatorAlbum = new ElementCreator(album_releaseParams);
+        this.viewElementCreator.addInnerElement(creatorAlbum);
+    }
+
+    /**
+     * @param {function} callback
+     */
+    setCallback(callback) {
+        if (typeof callback === 'function') {
+            this.callback = callback;
+        }
+    }
+
+    clickHandler() {
+        this.callback();
+    }
+
+    getSongInfo() {
+        return this.song;
+    }
+}
