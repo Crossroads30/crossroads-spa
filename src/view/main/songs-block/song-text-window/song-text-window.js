e22424108eeb9a0d53708c076ce992ace076e2dd
Crossroads30 <Crossroads30@mail.ru>
2023-07-26 17:13:08 +0300

    fix: rename file

---
+++ src/view/main/songs-block/song-text-window/song-text-window.js
@@ -0,0 +1,55 @@
+import ElementCreator from '../../../../util/element-creator';
+import SongView from '../songs/songs-list-view';
+import './song-text.css';
+
+const CssClasses = {
+    CONTAINER: 'text__conteiner',
+    FIELD: 'text__field',
+    TITLE: 'text__title',
+    BUTTON: 'back__button',
+};
+const BUTTON_BACK_TEXT = 'Назад...';
+
+export default class SongTextView extends SongView {
+    /**
+     * @param {import('../../../../data/songs/songs-list').SongData} song
+     */
+    configureView() {
+        this.viewElementCreator.setCssClasses([CssClasses.CONTAINER]);
+        this.viewElementCreator.element.classList.remove('song')
+
+        /**
+         * @type {import('../../../../util/element-creator').ElementParams}
+         */
+        let titleTextParams = {
+            tag: 'h3',
+            classNames: [CssClasses.TITLE],
+            textContent: `${this.song.name}`,
+            callback: null,
+        };
+        let creatorSongTitle = new ElementCreator(titleTextParams);
+        this.viewElementCreator.addInnerElement(creatorSongTitle);
+
+        /**
+         * @type {import('../../../../util/element-creator').ElementParams}
+         */
+        let songTextParams = {
+            tag: 'p',
+            classNames: [CssClasses.FIELD],
+            textContent: '',
+            callback: null,
+        };
+        let creatorSongText = new ElementCreator(songTextParams);
+        this.viewElementCreator.addInnerElement(creatorSongText);
+        creatorSongText.element.innerText = `${this.song.text}`;
+
+        const buttonParams = {
+            tag: 'button',
+            classNames: [CssClasses.BUTTON],
+            textContent: BUTTON_BACK_TEXT,
+            callback: this.clickHandler.bind(this),
+        };
+        const creatorButton = new ElementCreator(buttonParams);
+        this.viewElementCreator.addInnerElement(creatorButton);
+    }
+}
