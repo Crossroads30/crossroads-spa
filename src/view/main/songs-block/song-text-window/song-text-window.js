1f1721df851f07300d711ec4db66d65f3eaecfff
Crossroads30 <Crossroads30@mail.ru>
2023-07-30 02:42:46 +0300

    feat: add player fnc

--- src/view/main/songs-block/song-text-window/song-text-window.js
+++ src/view/main/songs-block/song-text-window/song-text-window.js
@@ -6,7 +6,9 @@ const CssClasses = {
     CONTAINER: 'text__conteiner',
     FIELD: 'text__field',
     TITLE: 'text__title',
-    BUTTON: 'back__button',
+    BUTTON: 'back-btn',
+    PLAY: 'play-btn',
+    PAUSE: 'pause-btn',
 };
 const BUTTON_BACK_TEXT = 'Назад...';
 
@@ -15,13 +17,40 @@ export default class SongTextView extends SongView {
      * @param {import('../../../../data/songs/songs-list').SongData} song
      */
     configureView() {
+        const myAudio = new Audio();
+        myAudio.src = this.song.src;
+
         this.viewElementCreator.setCssClasses([CssClasses.CONTAINER]);
         this.viewElementCreator.element.classList.remove('song');
         this.callback = null;
-        
+
         /**
          * @type {import('../../../../util/element-creator').ElementParams}
          */
+        const playerParams = {
+            tag: 'button',
+            classNames: [CssClasses.PLAY],
+            textContent: '',
+            callback: null,
+        };
+        const playerCreator = new ElementCreator(playerParams);
+        this.viewElementCreator.addInnerElement(playerCreator);
+
+        let isPlay = false;
+        playerCreator.element.addEventListener('click', () => {
+            if (!isPlay) {
+                myAudio.play();
+                playerCreator.element.classList.add('pause-btn');
+                playerCreator.element.classList.remove('play-btn');
+                isPlay = true;
+            } else if (isPlay) {
+                myAudio.pause();
+                playerCreator.element.classList.add('play-btn');
+                playerCreator.element.classList.remove('pause-btn');
+                isPlay = false;
+            }
+        });
+
         let titleTextParams = {
             tag: 'h3',
             classNames: [CssClasses.TITLE],
@@ -31,9 +60,6 @@ export default class SongTextView extends SongView {
         let creatorSongTitle = new ElementCreator(titleTextParams);
         this.viewElementCreator.addInnerElement(creatorSongTitle);
 
-        /**
-         * @type {import('../../../../util/element-creator').ElementParams}
-         */
         let songTextParams = {
             tag: 'p',
             classNames: [CssClasses.FIELD],
@@ -52,5 +78,8 @@ export default class SongTextView extends SongView {
         };
         const creatorButton = new ElementCreator(buttonParams);
         this.viewElementCreator.addInnerElement(creatorButton);
+        creatorButton.element.addEventListener('click', () => {
+            myAudio.pause();
+        });
     }
 }
